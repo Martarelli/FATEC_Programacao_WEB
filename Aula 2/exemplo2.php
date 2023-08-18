4063ee91881a29316d3b1c17db3d29499e12f6a5
Martarelli <renanmartarelli@gmail.com>
2023-08-18 20:37:09 -0300

    classe conta

--- Aula 2/exemplo2.php
+++ Aula 2/exemplo2.php
@@ -6,12 +6,34 @@ class Conta
     private string $conta;
     private float $saldo;
 
+    public __construct(float $valor)
+    {
+        $this->agencia = "";
+        $this->conta = "";
+        $this->saldo = $valor;
+    }
+    public function depositar(float $valor)
+    {
+        $this->saldo += $valor;
+    }
+    public function sacar(float $valor)
+    {
+        if (($this->saldo > 0) && ($this->saldo >= $valor))
+        {
+            $this->saldo -= $valor;
+        } else {
+            echo "Saldo Insuficiente";
+        }
+    }
+
     public function verSaldo(){
         echo "Saldo atual: " . $this->saldo . "<br>";
     }
-    
+
 }
 
+$c1 = new Conta(1000);
+$c1->verSaldo();
 
 
 
