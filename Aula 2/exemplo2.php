94ca834f4ccf6d20d2a04ef27cbb3741f1c3d8bb
Martarelli <renanmartarelli@gmail.com>
2023-08-18 20:48:45 -0300

    conta corrente e conta poupanca

--- Aula 2/exemplo2.php
+++ Aula 2/exemplo2.php
@@ -6,18 +6,17 @@ class Conta
     private string $conta;
     private float $saldo;
 
-    public __construct(float $valor)
-    {
+    public function __construct (float $valor){
         $this->agencia = "";
         $this->conta = "";
         $this->saldo = $valor;
     }
-    public function depositar(float $valor)
-    {
+
+    public function depositar(float $valor){
         $this->saldo += $valor;
     }
-    public function sacar(float $valor)
-    {
+
+    public function sacar(float $valor){
         if (($this->saldo > 0) && ($this->saldo >= $valor))
         {
             $this->saldo -= $valor;
@@ -29,11 +28,42 @@ class Conta
     public function verSaldo(){
         echo "Saldo atual: " . $this->saldo . "<br>";
     }
+}
 
+class ContaPoupanca extends Conta {
+
+    public function depositar(float $valor){
+
+    }
+
+    public function depositar(float $valor){
+
+    }
+  
+}
+
+class ContaCorrente extends Conta {
+
+    private float $limite;
+    
+    public function depositar(float $valor){
+
+    }
+
+    public function depositar(float $valor){
+
+    }
+  
 }
 
 $c1 = new Conta(1000);
 $c1->verSaldo();
+$c1->depositar(500);
+$c1->verSaldo();
+$c1->sacar(800);
+$c1->verSaldo();
+
+
 
 
 
