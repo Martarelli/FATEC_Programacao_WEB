59e0ad13a33607595bfa8ad78d8b9412e230e5a5
Martarelli <renanmartarelli@gmail.com>
2023-08-18 22:06:24 -0300

    metodos construtores

--- Aula 2/exercicio2.php
+++ Aula 2/exercicio2.php
@@ -5,6 +5,12 @@ class Pessoa {
     protected string $cpf;
     protected string $email;
 
+    public function __construct(string $nome, string $cpf, string $email){
+        $this->nome = $nome;
+        $this->cpf = $cpf;
+        $this->email = $email;
+    }
+
 }
 
 class Professor extends Pessoa {
@@ -12,18 +18,41 @@ class Professor extends Pessoa {
     private int $cargaHoraria;
     private float $salario;
     private string $departamento;
+
+    public function __construct(string $nome, string $cpf, string $email, string $matricula, int $cargaHoraria, float $salario, string $departamento){
+        parent::__construct($nome,  $cpf,  $email);
+        $this->matricula = $matricula;
+        $this->cargaHoraria = $cargaHoraria;
+        $this->salario = $salario;
+        $this->departamento = $departamento;
+    }
+
 }
 
 class Funcionario extends Pessoa {
     private string $matricula;
     private string $regime;
     private float $salario;
+
+    public function __construct(string $nome, string $cpf, string $email, string $matricula, string $regime, float $salario){
+        parent::__construct($nome,  $cpf,  $email);
+        $this->matricula = $matricula;
+        $this->regime = $regime;
+        $this->salario = $salario;
+    }
 }
 
 class Aluno extends Pessoa {
     private string $ra;
     private string $curso;
-    private float $termo;
+    private string $termo;
+
+    public function __construct(string $nome, string $cpf, string $email, string $ra, string $curso, string $termo){
+        parent::__construct($nome,  $cpf,  $email);
+        $this->ra = $ra;
+        $this->curso = $curso;
+        $this->termo = $termo;
+    }
 }
 
 
