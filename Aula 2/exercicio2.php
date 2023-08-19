bd0c85dee63ebef0df460e6e9cd142a536c57b93
Martarelli <renanmartarelli@gmail.com>
2023-08-18 21:51:19 -0300

    inicio exercicio 2

---
+++ Aula 2/exercicio2.php
@@ -0,0 +1,27 @@
+<?php 
+
+class Pessoa {
+    protected string $nome;
+    protected string $cpf;
+    protected string $email;
+
+}
+
+class Professor {
+    private string $matricula;
+    private int $cargaHoraria;
+    private float $salario;
+    private string $departamento;
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
