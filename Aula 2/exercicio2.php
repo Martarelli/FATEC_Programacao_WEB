975100d11611db65c2734af476a2bd243f5a6eb0
Martarelli <renanmartarelli@gmail.com>
2023-08-18 21:57:17 -0300

    atributos das classe

--- Aula 2/exercicio2.php
+++ Aula 2/exercicio2.php
@@ -7,11 +7,23 @@ class Pessoa {
 
 }
 
-class Professor {
+class Professor extends Pessoa {
     private string $matricula;
     private int $cargaHoraria;
     private float $salario;
     private string $departamento;
+}
+
+class Funcionario extends Pessoa {
+    private string $matricula;
+    private string $regime;
+    private float $salario;
+}
+
+class Aluno extends Pessoa {
+    private string $ra;
+    private string $curso;
+    private float $termo;
 }
 
 
