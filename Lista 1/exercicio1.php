74cd0cb353b42821c277fa0e31b3a6116c760351
Martarelli <renanmartarelli@gmail.com>
2023-08-25 20:48:58 -0300

    atributos e classe construtora

--- Lista 1/exercicio1.php
+++ Lista 1/exercicio1.php
@@ -1,6 +1,26 @@
 <?php
 
+class Invoice {
 
+    private int $numeroItem;
+    private string $descricao;
+    private int $qtdItemCompra;
+    private float $precoUnitario;
+
+    public function __construct(int $numeroItem, string $descricao, int $qtdItemCompra, float $precoUnitario){
+
+        $this->numeroItem = $numeroItem;
+        $this->descricao = $descricao;
+        $this->qtdItemCompra = $qtdItemCompra;
+        $this->precoUnitario = $precoUnitario;
+        
+    }
+
+
+
+
+
+}
 
 
 ?>
