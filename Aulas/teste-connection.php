09eed032dd954223fa36e2b6dd9a90c83db3033e
Martarelli <renanmartarelli@gmail.com>
2023-08-25 22:36:09 -0300

    teste de conexão

--- Aulas/teste-connection.php
+++ Aulas/teste-connection.php
@@ -4,11 +4,18 @@ require_once('connection.php');
 
 $str = "SELECT * from contatos";
 
+$conn->beginTransaction();
+
 $dados = $conn->query($str);
 
 $conn->exec("INSERT INTO contatos(nome, email) VALUES ('João', 'joao@teste.com')");
+
 $conn->exec("UPDATE contatos SET email = 'joaosilva@teste.com' WHERE nome = João");
 
+//se tudo correu bem
+$conn->commit();
+//se não
+$conn->rollback();
 
 
 
