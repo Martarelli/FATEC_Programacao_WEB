b112937a4c792403155f7a4c5dd1e961291ef270
Martarelli <renanmartarelli@gmail.com>
2023-08-27 20:54:07 -0300

    enunciado do exercicio

--- Lista 1/exercicio05.php
+++ Lista 1/exercicio05.php
@@ -1,5 +1,10 @@
 <?php  
 
-
+/* Exercício 05: Escreva uma classe que represente um círculo no plano cartesiano. Forneça os seguintes membros de classe:
+• Um construtor que inicialize o ponto em um local por parâmetros ou na origem do espaço;
+• Métodos getter e setter.
+• Métodos de inflar e desinflar que respectivamente aumenta e diminuem o tamanho do círculo.
+• Método para mover o círculo a partir de outro ponto ou para a origem do espaço.
+• Método que retorna a área do círculo. */
 
 ?>
