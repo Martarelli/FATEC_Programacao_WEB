a2638c4b2e711166ee5c1c83caa996a1d0da4faa
Martarelli <renanmartarelli@gmail.com>
2023-08-27 18:59:25 -0300

    exercicio 2 finalizado

--- Lista 1/exercicio02.php
+++ Lista 1/exercicio02.php
@@ -6,5 +6,55 @@
     • um salário mensal.
     Sua classe deve ter um construtor que inicializa os três atributos. Forneça um método set e get para cada atributo. Se o salário mensal não for positivo, configure-o como 0.0. Crie um método que exibe o salário anual e um que dê 10% de aumento no salário. */
 
+class Empregado {
+
+    private string $nome;
+    private string $sobrenome;
+    private float $salario;
+
+    public function __construct(string $nome, string $sobrenome, float $salario){
+        $this->SetNome($nome);
+        $this->SetSobrenome($sobrenome);
+
+        if ($salario >= 0) {
+            $this->SetSalario($salario);
+        } else {
+            $this->SetSalario(0);
+        }
+    }
+    
+    public function GetNome(){
+        return $this->nome;
+    }
+    public function SetNome($nome){
+        $this->nome = $nome;
+    }
+    
+    public function GetSobrenome(){
+        return $this->sobrenome;
+    }
+    public function SetSobrenome($sobrenome){
+        $this->sobrenome = $sobrenome;
+    }
+
+    public function GetSalario(){
+        return $this->salario;
+    }
+    public function SetSalario($salario){
+        $this->salario = $salario;
+    }
+
+    public function Aumento(){
+        $newSalario = $this->GetSalario() * 1.1;
+        $this->SetSalario($newSalario);
+        return $this->GetSalario();
+    }
+}
+
+$func1 = new Empregado("Renan", "Martarelli", 1000.00);
+echo "<p>". $func1->GetNome() . "</p>";
+echo "<p>". $func1->GetSobrenome() . "</p>";
+echo "<p>". $func1->GetSalario() . "</p>";
+echo "<p>". $func1->Aumento() . "</p>";
 
 ?>
