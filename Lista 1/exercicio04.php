97834573a02080d69f8122c41cb552d61ddd2a75
Martarelli <renanmartarelli@gmail.com>
2023-08-27 20:49:37 -0300

    exercicio 4 finalizado

--- Lista 1/exercicio04.php
+++ Lista 1/exercicio04.php
@@ -16,7 +16,7 @@ class Ponto2D {
             $this->SetX($x);
             $this->SetY($y);
     }
-
+    
     public function GetX(){
         return $this->x;
     }
@@ -29,11 +29,49 @@ class Ponto2D {
     public function SetY($y){
         $this->y = $y;
     }
+
+    public function SetPonto($x, $y) {
+        $this->SetX($x);
+        $this->SetY($y);
+    }
+
+    public function ComparaPontos($outroPonto) {
+        if($this->GetX() == $outroPonto->GetX() && $this->GetY() == $outroPonto->GetY()){
+            return "Mesmo Ponto";
+        } else {
+            return "Ponto Diferente";
+        }
+    }
+
+    public function DistanciaDoisPontos($Ponto) {
+        $distX = $this->GetX() - $Ponto->GetX();
+        if($distX < 0){
+            $distX *= -1;
+        }
+
+        $distY = $this->GetY() - $Ponto->GetY();
+        if($distY < 0){
+            $distY *= -1;
+        }
+
+        return sqrt($distX * $distX + $distY * $distY);
+    }
+
+    public function ClonePonto() {
+        return new Ponto2D($this->GetX(), $this->GetY());
+    }
 }
 
 $p1 = new Ponto2D(1, 5);
 $p2 = new Ponto2D();
+$p3 = $p2->ClonePonto();
 
 echo "<p>".$p1->GetX().",".$p1->GetY()."</p>";
 echo "<p>".$p2->GetX().",".$p2->GetY()."</p>";
+
+$p1->SetPonto(10,12);
+echo "<p>" . $p2->ComparaPontos($p1) . "</p>";
+echo "<p>" . $p2->ComparaPontos($p3) . "</p>";
+echo "<p>" . $p1->DistanciaDoisPontos($p3) . "</p>";
+
 ?>
