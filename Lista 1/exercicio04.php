9aeccd8f27fe505c62e182ccd3095067978658c3
Martarelli <renanmartarelli@gmail.com>
2023-08-27 19:19:40 -0300

    exercicio 4 finalizado

---
+++ Lista 1/exercicio04.php
@@ -0,0 +1,13 @@
+<?php 
+/* Exercício 04: Escreva uma classe Ponto2D que represente um ponto no plano cartesiano. Além dos atributos por você identificados, a classe deve oferecer os seguintes membros:
+    • Construtor que permita a inicialização do ponto na origem ou em um local informado por parâmetros.
+    • Método para definir o local do ponto a partir de outro ponto;
+    • Método de comparação de pontos;
+    • Métodos de acesso getter e setter.
+    • Método que permita calcular a distância do ponto que recebe a mensagem para outro.
+    • Método que permita a criação de um novo ponto no mesmo local do ponto que recebeu a mensagem(clone).
+ */
+
+
+
+?>
