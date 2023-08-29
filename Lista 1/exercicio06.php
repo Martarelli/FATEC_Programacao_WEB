275e2b49cc0bbb6f563621ac6961fff0e3405af6
Martarelli <renanmartarelli@gmail.com>
2023-08-29 20:40:07 -0300

    inicio exercicio 6

---
+++ Lista 1/exercicio06.php
@@ -0,0 +1,14 @@
+<?php 
+
+/* Exercício 06: Crie uma classe para representar datas. Represente uma data usando três
+atributos: o dia, o mês, e o ano.
+• Sua classe deve ter um construtor que inicializa os três atributos e verifica a
+validade dos valores fornecidos.
+• Forneça um método set um get para cada atributo.
+• Forneça o método date para retornar uma representação da data. Considere que
+a data deve ser formatada mostrando o dia, o mês e o ano separados por barra
+(/).
+• Forneça uma operação para avançar uma data para o dia seguinte.  */
+
+
+?>
