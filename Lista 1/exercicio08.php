9f6f4b17d10fe94f5331e45ca0ffa8b78cb3654f
Martarelli <renanmartarelli@gmail.com>
2023-09-01 14:35:13 -0300

    exercicio 8 iniciado

---
+++ Lista 1/exercicio08.php
@@ -0,0 +1,12 @@
+<?php 
+
+/* Exercício 08: Escreva uma classe que represente um país. Um país tem como atributos o seu nome, o nome da capital, sua dimensão em Km2 e uma lista de países com os quais ele faz fronteira. Represente a classe e forneça os seguintes construtores e métodos:
+    • Construtor que inicialize o nome, capital e a dimensão do país;
+    • Métodos de acesso (obter/get) para as propriedades indicadas no item (a);
+    • Um método que permita verificar se dois países são iguais. Dois países são iguais se tiverem o mesmo nome e a mesma capital.
+    • Um método que define quais outros países fazem fronteira (note que um país não pode fazer fronteira com ele mesmo);
+    • Um método que retorne a lista de países que fazem fronteira;
+    • Um método que receba um outro país como parâmetro e retorne uma lista de vizinhos comuns aos dois países. */
+
+
+?>
