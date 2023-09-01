4abb3e6c45b1ead54e1b5098110958fdd34b9eb1
Martarelli <renanmartarelli@gmail.com>
2023-09-01 14:35:06 -0300

    exercicio 7 iniciado

---
+++ Lista 1/exercicio07.php
@@ -0,0 +1,12 @@
+<?php 
+
+/* Exercício 07: Traduza o seguinte conjunto de classes em um programa PHP.
+a)Classe: Porta - Atributos: aberta, cor, dimensaoX, dimensaoY, dimensaoZ Métodos: void abre(), void fecha(), void pinta(String s).
+
+b)Classe: Casa - Atributos: cor, porta1, porta2, porta3 Métodos: void pinta(String s),totalDePortas ) int quantasPortasEstaoAbertas() int.
+
+c)Classe: Edifício -  Atributos: cor, totalDePortas, totalDeAndares, portas[] Métodos: void pinta(String s), int quantasPortasEstaoAbertas(), void adicionaPorta(Porta p), int totalDePortas(), void adicionarAndar(), int totalDeAndares().
+
+d)As classes Casa e edifício ficaram muito parecidas. Crie a classe Imóvel e coloque nela tudo o Casa e Edifício tem em comum. Faça Imóvel superclasse de Casa e Edifício.    */
+
+?>
