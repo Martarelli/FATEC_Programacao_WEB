7240eb663a9251eba12c0f6b8d932561a7ea5b01
Martarelli <renanmartarelli@gmail.com>
2023-09-02 16:44:10 -0300

    exercicio 6 finalizado

--- Lista 1/exercicio06.php
+++ Lista 1/exercicio06.php
@@ -16,16 +16,17 @@ class Datas {
     private int $ano;
 
     public function __construct(int $dia = 1, int $mes = 1, int $ano = 2023){
+        
         if($dia >= 1 && $dia <= 31){
             $this->setDia($dia);
         }
 
         if($mes >= 1 && $mes <= 12){
-            $this->setDia($mes);
+            $this->setMes($mes);
         }
 
         if($ano >= 1 && $ano <= 9999){
-            $this->setDia($ano);
+            $this->setAno($ano);
         }
     }
     
@@ -59,6 +60,28 @@ class Datas {
         $this->ano = $ano;
     }
 
+    public function date(){
+        $data = $this->getDia() . "/" . $this->getMes() ."/". $this->getAno();
+        return $data;
+    }
 
+    public function nextDay(){
+        if($this->getDia() <= 30){
+            $this->setDia($this->getDia() + 1);
+        } else if ($this->getMes() <= 11) {
+            $this->setDia(1);
+            $this->setMes($this->getMes() + 1);
+        } else {
+            $this->setDia(1);
+            $this->setMes(1);
+            $this->setAno($this->getAno() + 1);
+        }
+    }
 }
+
+$dt1 = new Datas(31,12,2000);
+echo  "<p>" .$dt1->date() . "</p>";
+$dt1->nextDay();
+echo  "<p>" .$dt1->date() . "</p>";
+
 ?>
