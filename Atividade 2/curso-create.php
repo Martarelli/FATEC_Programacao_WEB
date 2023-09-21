5c664761cae19d531577dbe314d5f65813ad6dcb
Martarelli <renanmartarelli@gmail.com>
2023-09-21 16:49:03 -0300

    create curso

---
+++ Atividade 2/curso-create.php
@@ -0,0 +1,79 @@
+<?php 
+require_once "header_inc.php";
+require_once "connection.php";
+
+$nome = '';
+$descricao = '';
+$imagem = '';
+$flag_msg = null;
+if(isset($_POST['enviar'])){
+
+    if (empty($_POST["nome"]) || empty($_POST["descricao"]) || empty($_POST["imagem"])) {
+        $flag_msg = false; //Erro 
+        $msg = "Dados incompletos, preencha o formulário corretamente!";
+    } else {
+        $nome = $_POST['nome'];
+        $descricao = $_POST['descricao'];
+        $imagem = $_POST['imagem'];
+        $dataCadastro = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO cursos (nome, descricao, imagem, dataCadastro) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $descricao);
+        $stmt->bindParam(3, $imagem);
+        $stmt->bindParam(4, $dataCadastro);
+        
+        $stmt->execute();
+
+        $flag_msg = true; // Sucesso 
+        $msg = "Dados enviados com sucesso!";
+        $nome = "";
+        $descricao = "";
+        $imagem = "";
+
+    }
+
+    $conn = null;
+}
+?>
+<div class="p-4 mb-4 bg-light">
+  <h1 class="display-5">Cursos</h1>
+  <hr class="my-3">
+  <p class="lead">Nossa equipe está sempre a disposição para ouvir as suas críticas e sugestões. Entre em contato que iremos responder o mais breve possível.</p>
+</div>
+
+<div class="container">
+  <?php 
+    if (!is_null($flag_msg)) {
+      if ($flag_msg) {
+        echo "<div class='alert alert-success' role='alert'>$msg</div>"; 
+      }else{
+        echo "<div class='alert alert-warning' role='alert'>$msg</div>"; 
+      }
+    }
+  ?>
+  <form method="POST">
+    <div class="form-group">
+      <label for="nome">Nome:</label>
+      <input type="text" class="form-control" id="nome" name="nome" value="<?= $nome; ?>" required>
+    </div>
+    <br />
+    <div class="form-group">
+      <label for="descricao">Descrição:</label>
+      <input type="text" class="form-control" id="descricao" name="descricao" value="<?= $descricao; ?>">
+    </div>
+    <br />
+    <div class="form-group">
+      <label for="imagem">Imagem:</label>
+      <input type="text" class="form-control" id="imagem" name="imagem" value="<?= $imagem; ?>">
+    </div>
+    <br />
+
+    <br />
+    <button type="submit" class="btn btn-primary mb-2" name="enviar">Enviar</button>
+    <a href="curso-list.php"><button type="button" class="btn btn-primary mb-2" name="limpar">Limpar</button></a>
+  </form>
+</div>
+
+<?php require_once "footer_inc.php"; ?>
