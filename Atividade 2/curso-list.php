3a3135a20a1ee0a4aee4464118a748685baff730
Martarelli <renanmartarelli@gmail.com>
2023-09-21 16:30:06 -0300

    exibição cursos area adm

--- Atividade 2/curso-list.php
+++ Atividade 2/curso-list.php
@@ -1,4 +1,12 @@
-<?php require_once "header_inc.php"; ?>
+<?php
+
+require_once "header_inc.php"; 
+require_once "curso.php";
+
+$cursos = Curso::listarCursos();
+
+
+?>
 
 <div class="p-4 mb-4 bg-light">
   <h1 class="display-5">Gerenciamento de Cursos</h1>
@@ -7,7 +15,7 @@
 </div>
 
 <div class="container">
-  <a class="btn btn-success" href="#">Criar Novo Curso</a>
+  <a class="btn btn-success" href="curso-create.php">Criar Novo Curso</a>
   <p></p>
   <table class="table table-bordered">
     <tr class="table-success text-center text-center">
@@ -18,6 +26,19 @@
       <th>Imagem</th>
       <th>Ação</th>
     </tr>
+    <?php foreach ($cursos as $curso) { ?>
+    <tr class="text-center">
+      <td class="table-light"> <?= $curso->getId()?> </td>
+      <td class="table-light"> <?= $curso->getDataCadastro()?> </td>
+      <td class="table-light"> <?= $curso->getNome()?> </td>
+      <td class="table-light"> <?= $curso->getDescricao()?> </td>
+      <td class="table-light"> <?= $curso->getImagem()?> </td>
+      <td class="table-light" style="width: 15%">
+        <a href="curso-show.php?id=<?= $curso->getId(); ?>"><button type="button" class="btn btn-primary">Exibir</button></a>
+        <a href="curso-destroy.php?id=<?= $curso->getId(); ?>"><button type="button" class="btn btn-danger">Excluir</button></a>
+      </td>
+    </tr>
+    <?php  }?>
   </table>
 </div>
 
