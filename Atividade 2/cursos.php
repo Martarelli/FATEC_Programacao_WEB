72744599122b882b7b1a270aa9cf684b7ffbd673
Martarelli <renanmartarelli@gmail.com>
2023-09-21 16:19:51 -0300

    listagem da pagina de cursos

--- Atividade 2/cursos.php
+++ Atividade 2/cursos.php
@@ -1,4 +1,14 @@
-<?php require_once "header_inc.php"; ?>
+<?php 
+
+require_once "header_inc.php"; 
+require_once "curso.php";
+
+$cursos = Curso::listarCursos();
+
+
+
+
+?>
 
 <div class="p-4 mb-4 bg-light">
   <h1 class="display-5">Cursos</h1>
@@ -8,19 +18,23 @@
 
 <br />
 <div class="container">
+  <?php foreach ($cursos as $curso) { ?>
+    
+
   <div class="row featurette">
     <div class="col-md-7">
-      <h2 class="featurette-heading">Curso de PHP</h2>
-      <p class="lead">Curso de PHP. Aprenda a linguagem Web mais popular da internet em poucas aulas.</p>
+      <h2 class="featurette-heading"><?= $curso->getNome()?></h2>
+      <p class="lead"><?= $curso->getDescricao()?></p>
       <p class="lead"><a href="#"><button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Saiba mais</button></a></p>
     </div>
     <div class="col-md-5">
       <figure class="figure">
-          <img src="images/img-curso-php.png" class="figure-img img-fluid rounded" alt="Curso de PHP">
+          <img src=<?= $curso->getImagem()?> class="figure-img img-fluid rounded" alt="Curso de PHP">
       </figure>
     </div>
   </div>
   <hr class="featurette-divider">
+  <?php  }?>
 </div>
 
 <?php require_once "footer_inc.php"; ?>
