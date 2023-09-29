462059ac2b09e263afda99e7ccaafc3c2df9abd0
Martarelli <renanmartarelli@gmail.com>
2023-09-29 09:35:24 -0300

    script SQL

---
+++ Tasks_RenanMartarelli/Tasks_RenanMartarelli.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS Tasks_RenanMartarelli;
+
+CREATE DATABASE Tasks_RenanMartarelli;
+
+CREATE TABLE IF NOT EXISTS Status (
+    id INT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    ordem VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    atividade TEXT,
+    prioridade INT,
+    dataInicio DATE,
+    dataConclusao DATE,
+    dataCadastro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status_id INT,
+    FOREIGN KEY (status_id) REFERENCES Status(id)
+);
