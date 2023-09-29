2de364c51adaacbee0a54e894a6386e8701011b5
Martarelli <renanmartarelli@gmail.com>
2023-09-29 09:59:46 -0300

    class usuario

---
+++ MVC/App/Models/Entidades/Usuario.php
@@ -0,0 +1,95 @@
+<?php
+
+class Usuario {
+    private $id;
+    private $nome;
+    private $email;
+    private $username;
+    private $password;
+    private $tipo;
+    private $data_cadastro;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function setUsername($username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo($tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    public function getDataCadastro()
+    {
+        return $this->data_cadastro;
+    }
+
+    public function setDataCadastro($data_cadastro): self
+    {
+        $this->data_cadastro = $data_cadastro;
+
+        return $this;
+    }
+}
