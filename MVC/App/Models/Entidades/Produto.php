3dfb3f343784dadbc2a6da7e069094b634a4b3c9
Martarelli <renanmartarelli@gmail.com>
2023-10-06 21:44:23 -0300

    classe produto

---
+++ MVC/App/Models/Entidades/Produto.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Models\Entidades;
+
+use DateTime;
+
+class Produto 
+{
+    private int $id;
+    private string $nome;
+    private float $preco;
+    private float $quantidade;
+    private string $descricao;
+    private string $dataCadastro;
+    private string $imagem;
+    private Fornecedor $fornecedor;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function setPreco($preco)
+    {
+        $this->preco = $preco;
+    }
+
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    public function setQuantidade($quantidade)
+    {
+        $this->quantidade = $quantidade;
+    }
+
+    public function getDescricao()
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao($descricao)
+    {
+        $this->descricao = $descricao;
+    }
+
+    public function getDataCadastro()
+    {
+        return new DateTime($this->dataCadastro);
+    }
+
+    public function setDataCadastro($dataCadastro)
+    {
+        $this->dataCadastro = $dataCadastro;
+    }
+
+    public function getFornecedor()
+    {
+        return $this->fornecedor;
+    }
+
+    public function setFornecedor(Fornecedor $fornecedor)
+    {
+        $this->fornecedor = $fornecedor;
+    }
+
+    public function getImagem()
+    {
+        return $this->imagem;
+    }
+
+    public function setImagem($imagem)
+    {
+        $this->imagem = $imagem;
+    }
+}
